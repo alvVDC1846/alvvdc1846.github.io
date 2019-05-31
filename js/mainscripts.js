588ad5e8384ff85d149aa3658619ff920d326c51
Alvaro Garcia <alvvdc@gmail.com>
2019-05-31 08:26:20 +0200

    Added GPL license 	new file:   COPYING 	modified:   css/mainstyles.css 	modified:   index.html 	modified:   js/mainscripts.js

--- js/mainscripts.js
+++ js/mainscripts.js
@@ -1,3 +1,18 @@
+/* This file is part of ALVVDC1846 Portfolio.
+
+ALVVDC1846 Portfolio is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ALVVDC1846 Portfolio is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ALVVDC1846 Portfolio.  If not, see <https://www.gnu.org/licenses/>. */
+
 $(document).foundation();
 
 $(document).ready(function () {
